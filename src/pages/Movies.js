17968b607a91f637fd450d2ae4e92d89f597790a
Dzenkinstone <kozlovdanil6@gmail.com>
2023-02-08 21:47:33 +0100

    changes

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,3 +1,4 @@
+import toast, { Toaster } from 'react-hot-toast';
 import { getMovieByQuery } from 'api';
 import { FoundMovies } from 'components/FoundMovies';
 import { MoviesList } from 'components/MoviesList';
@@ -25,8 +26,13 @@ const Movies = () => {
       try {
         const responce = await getMovieByQuery(name);
 
+        if (responce.total_results === 0) {
+          toast('Sorry, there is no films with that name', {
+            duration: 2000,
+            style: { color: 'red' },
+          });
+        }
         setMovies(responce.results);
-        console.log(responce);
       } catch (error) {
         console.log(error);
       }
@@ -38,6 +44,7 @@ const Movies = () => {
   return (
     <main>
       <FoundMovies onSubmit={handleSubmit} />
+      <Toaster />
       {movies && <MoviesList movies={movies} />}
     </main>
   );
